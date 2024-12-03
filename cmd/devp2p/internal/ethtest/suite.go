430a06544abfb0d92a478d4732483c59a995d8bc
fkrause98 <fkrausear@gmail.com>
2024-12-03 13:13:59 -0300

    test: try with legacy receipt

--- cmd/devp2p/internal/ethtest/suite.go
+++ cmd/devp2p/internal/ethtest/suite.go
@@ -391,8 +391,8 @@ func (s *Suite) TestGetBlockReceipts(t *utesting.T) {
 	req := &eth.GetReceiptsPacket{
 		RequestId: 55,
 		GetReceiptsRequest: eth.GetReceiptsRequest{
-			// s.chain.blocks[54].Hash(),
-			s.chain.blocks[75].Hash(),
+			s.chain.blocks[54].Hash(),
+			// s.chain.blocks[75].Hash(),
 		},
 	}
 
